9a9607f7b39daf62f1576a1a0fb70542177f32fa
Guilherme Pimentel <ggampp@gmail.com>
2017-01-26 22:48:28 -0200

    Commit inicial

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var http    = require('https');
+var app = express();
+
+const apiKey = 'a88281bd9cd04d5aacc542124b4dca63';
+
+app.use(express.static('app'));
+
+app.get('/news/:id/:sortBy', function(req, res){
+
+        var options = {
+            host: 'newsapi.org',
+            path: '/v1/articles?source='+req.params.id+'&sortBy='+req.params.sortBy+'&apiKey=' + apiKey
+        };
+        var self = res;
+
+        callback = function (response) {
+            var str = '';
+            var str = '';
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+            response.on('end', function () {
+                var parsed = JSON.parse(str);
+                
+                self.setHeader('Content-Type', 'application/json');
+                self.send(str);
+            });
+        }
+        http.request(options, callback).end();
+
+});
+
+app.listen(3000, function(){
+    console.log("Ó o Gás!!!! na porta 3000.");
+});
---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+$(function () {
+
+  $.getJSON( "/news/espn/top", function( data ) {
+      var theTemplateScript = $("#inf-card-template").html();
+      var theTemplate = Handlebars.compile(theTemplateScript);
+      var theCompiledHtml = theTemplate(data);
+      $('#cards').append(theCompiledHtml);    
+  });
+
+
+});
