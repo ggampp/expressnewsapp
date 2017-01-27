91d14e7c724ee9b57bb325bd2c96862ab3f5d13f
Guilherme Pimentel <ggampp@gmail.com>
2017-01-27 00:00:01 -0200

    Commit inicial

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,8 @@
 $(function () {
-
   $.getJSON( "/news/espn/top", function( data ) {
       var theTemplateScript = $("#inf-card-template").html();
       var theTemplate = Handlebars.compile(theTemplateScript);
       var theCompiledHtml = theTemplate(data);
       $('#cards').append(theCompiledHtml);    
   });
-
-
 });
