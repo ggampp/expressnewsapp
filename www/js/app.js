6f9dbf0aca30aad81db49103c37cdf509d3a1034
Guilherme Pimentel <ggampp@gmail.com>
2017-01-28 00:05:21 -0200

    Commit inicial

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,28 @@
 $(function () {
-  $.getJSON( "/news/espn/top", function( data ) {
+
+  loadNews("/espn/top", false);
+
+  $.getJSON( "/sources/en/us/business", function( data ) {
+      var theTemplateScript = $("#menu-template").html();
+      var theTemplate = Handlebars.compile(theTemplateScript);
+      var theCompiledHtml = theTemplate(data);
+      $('#menu').append(theCompiledHtml);
+
+      $('.link li').click(function (e) {
+  		    e.preventDefault();
+          var link = $(this).data('link');
+          loadNews(link, true);
+	    });
+  });
+});
+
+function loadNews(link, closeMenu){
+  $.getJSON( "/news" + link, function( data ) {
+      $('#cards').empty();
       var theTemplateScript = $("#inf-card-template").html();
       var theTemplate = Handlebars.compile(theTemplateScript);
       var theCompiledHtml = theTemplate(data);
-      $('#cards').append(theCompiledHtml);    
+      $('#cards').append(theCompiledHtml);
+      if (closeMenu) $('.hamburger').click();    
   });
-});
+}
